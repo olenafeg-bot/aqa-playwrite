99812777a4b5f5f9e6fdcf8911f3d7b0bf727535
Olena Fehetsyn <olena.fehetsyn@medtronic.com>
2026-04-10 17:04:30 +0300

    Hometask 26. CLI and ENV

--- playwright.config.js
+++ playwright.config.js
@@ -5,15 +5,21 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
+import dotenv from 'dotenv';
 // import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+ dotenv.config();
+
+ function loadEnvFile() {
+  const envFile = process.env.ENV_FILE || '.env';
+  dotenv.config({ path: `.env${envFile}` });
+ }
 
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
   testDir: './tests',
+  
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -27,8 +33,11 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
-
+    baseURL: process.env.BASE_URL ?? 'https://qauto.forstudy.space/',
+    httpCredentials: {
+      username: process.env.HTTP_USERNAME ?? 'guest',
+      password: process.env.HTTP_PASSWORD ?? 'welcome2auto',
+    },
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
